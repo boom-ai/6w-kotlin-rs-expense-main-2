9cde8a8a6e5e33af12dbef3794d14fc2a3cf0160
boom-ai <69669572+boom-ai@users.noreply.github.com>
2023-08-07 17:33:19 +0530

    update

--- app/src/main/java/com/rajibul/expensetracker/InputFragment.kt
+++ app/src/main/java/com/rajibul/expensetracker/InputFragment.kt
@@ -1,10 +1,12 @@
 package com.rajibul.expensetracker
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,8 @@ class InputFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     lateinit var mainActivity: MainActivity
+    var tvCredit:TextView?=null
+    var tvDebit:TextView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +41,16 @@ class InputFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvCredit=view.findViewById(R.id.tvCredit)
+        tvCredit?.setOnClickListener{
+            var dialog= Dialog(mainActivity)
+            dialog.setContentView(R.layout.add_transaction)
+            dialog.show()
+        }
     }
 
     companion object {
